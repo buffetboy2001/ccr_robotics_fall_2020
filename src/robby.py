f7a7d287a121a20381b92cee35bc2f1fc893dadf
bowman.d.stuart@gmail.com <bowman.d.stuart@gmail.com>
2020-06-03 22:56:37 -0400

    starting robot script based on instructions, which seem to be slightly wrong

---
+++ src/robby.py
@@ -0,0 +1,11 @@
+# Write your code here :-)
+from gpiozero import Robot
+import time
+
+robby = Robot(left=(7,8), right=(9,10))
+
+# robby.forward()  # async
+# robby.right(1)
+# robby.left(1)
+# time.sleep(5)
+# robby.stop() # async
