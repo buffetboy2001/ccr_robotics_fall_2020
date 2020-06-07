2f91a87c017bb53727c15d03aeef530b8d03e522
bowman.d.stuart@gmail.com <bowman.d.stuart@gmail.com>
2020-06-06 23:23:06 -0400

    it works!

--- src/robby.py
+++ src/robby.py
@@ -5,8 +5,8 @@ import time
 robby = Robot(left=(7,8), right=(9,10))
 
 # robby.forward()  # async
-robby.backward()
-# robby.right(1)
+# robby.backward()
+robby.right(1)
 # robby.left(1)
-time.sleep(10)
+time.sleep(3)
 robby.stop() # async
