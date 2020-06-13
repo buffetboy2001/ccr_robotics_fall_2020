1ec77b9d5492e079900406dcebaf854195a9bac1
bowman.d.stuart@gmail.com <bowman.d.stuart@gmail.com>
2020-06-13 14:44:19 -0400

    calibrated 90 deg turn

---
+++ src/90_deg_turn.py
@@ -0,0 +1,12 @@
+# Write your code here :-)
+from gpiozero import Robot
+import time
+
+robby = Robot(left=(8, 7), right=(9, 10))
+
+sleep_time_sec = 0.7
+turn_speed = 0.5 
+
+robby.right(turn_speed)  # async
+time.sleep(sleep_time_sec)
+robby.stop() 
