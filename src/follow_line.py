caeadeff717ae61108485084af151e0dd7a0a61e
bowman, stuart <bowman.d.stuart@gmail.com>
2020-07-05 21:43:08 -0400

    isolating left sensor for now

--- src/follow_line.py
+++ src/follow_line.py
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     # Setup Robby
     robby = setup.get_robby()
     left_sensor = LineSensor(21)
-    right_sensor = LineSensor(2)
+    # right_sensor = LineSensor(2)
 
     left_sensor.when_line = lambda: print('Line detected @ left sensor') # left_line_detected_response()
-    right_sensor.when_line = lambda: print('Line detected @ right sensor') # right_line_detected_response()
+    # right_sensor.when_line = lambda: print('Line detected @ right sensor') # right_line_detected_response()
     left_sensor.when_no_line = lambda: print('No line detected') # when_no_line_do_this()
-    right_sensor.when_no_line = lambda: print('No line detected') # when_no_line_do_this()
+    # right_sensor.when_no_line = lambda: print('No line detected') # when_no_line_do_this()
 
     pause()
     
