562ca11290842664592f43ccb733dac651f4a08b
bowman, stuart <bowman.d.stuart@gmail.com>
2020-07-05 22:19:56 -0400

    now using forward function

--- src/follow_line.py
+++ src/follow_line.py
@@ -2,7 +2,9 @@ import setup
 from gpiozero import LineSensor
 from signal import pause
 from time import sleep
- 
+
+robby = None
+
 def left_line_detected_response():
     pass
 
@@ -10,7 +12,8 @@ def right_line_detected_response():
     pass
 
 def when_no_line_do_this():
-    pass
+    robby.forward()
+    return
 
 if __name__ == "__main__":
     '''
@@ -23,8 +26,8 @@ if __name__ == "__main__":
     right_sensor = LineSensor(14)
 
     # line detected behavior
-    left_sensor.when_no_line = lambda: print('Line detected @ left sensor') # left_line_detected_response()
-    right_sensor.when_no_line = lambda: print('Line detected @ right sensor') # right_line_detected_response()
+    left_sensor.when_no_line = when_no_line_do_this
+    right_sensor.when_no_line = when_no_line_do_this
 
     # no line detected behavior
     left_sensor.when_line = lambda: print('No line detected') # when_no_line_do_this()
