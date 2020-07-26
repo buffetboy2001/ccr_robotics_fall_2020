5d62709aa863cf3cbdc73bae8d1c44d7e2655076
Stuart Bowman <bowman.d.stuart@gmail.com>
2020-07-25 22:29:37 -0400

    new code for line following; not significant change

--- src/follow_line.py
+++ src/follow_line.py
@@ -3,24 +3,27 @@ from gpiozero import LineSensor
 from signal import pause
 from time import sleep
 
+DEADBAND_DURATION_SEC = 0.2
+FORWARD_SPEED = 0.5 # below 0.5 and there is too much friction to move
+
 robby = None
 
 def left_line_detected_response():
     print('turn left')
     robby.forward(curve_left=0.85)
-    sleep(.2)
-    robby.forward(0.5)
+    sleep(DEADBAND_DURATION_SEC)
+    robby.forward(FORWARD_SPEED)
     return
 
 def right_line_detected_response():
     print('turn right')
     robby.forward(curve_right=0.85)
-    sleep(.2)
-    robby.forward(0.5)
+    sleep(DEADBAND_DURATION_SEC)
+    robby.forward(FORWARD_SPEED)
     return
 
 def when_no_line_do_this():
-    robby.forward(.5)
+    robby.forward(FORWARD_SPEED)
     return
 
 if __name__ == "__main__":
@@ -42,7 +45,7 @@ if __name__ == "__main__":
     right_sensor.when_line = right_line_detected_response
 
     # start by going forward
-    robby.forward(0.5)
+    robby.forward(FORWARD_SPEED)
     pause()
     
     print("done")
