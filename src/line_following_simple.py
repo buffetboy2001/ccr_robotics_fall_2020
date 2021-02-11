c8728d10fcd096322dbc069c6ee1dfc2c6ae39f2
Stuart Bowman <bowman.d.stuart@gmail.com>
2021-02-10 20:08:21 -0500

    added simplest form of line following

---
+++ src/line_following_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from gpiozero import Robot, LineSensor
+from signal import pause
+from time import sleep
+
+robby = Robot(left=(7, 8), right=(9, 10))
+left_sensor = LineSensor(17)
+right_sensor = LineSensor(27)
+
+left_sensor.when_line = robby.left
+left_sensor.when_no_line = robby.forward
+
+right_sensor.when_line = robby.right
+right_sensor.when_no_line = robby.forward
+
+# pause()
+sleep(10)
